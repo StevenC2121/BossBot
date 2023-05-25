1bfa995c3d9d56fa694c17a18eccbf7dbe6cbde5
StevenC2121 <stevencabral2121@gmail.com>
2023-05-25 16:33:01 -0400

    Updates odds command for NFL games
    The bot can now be queried to return the odds of the next scheduled NFL game for any team. Still working on getting the date working but the odds are outputting as expected.

--- commands/betting-odds.js
+++ commands/betting-odds.js
@@ -5,29 +5,92 @@ const api_key = process.env.ODDSTOKEN;
 
 module.exports = new Command('odds', async (message, args) => {
     try {
-        const response = await got.get(`https://api.the-odds-api.com/v4/sports/?apiKey=${api_key}`, {
+        if (!args || args.length < 2) {
+            message.channel.createMessage('Please provide a sport code and a team name! Example: odds nfl chiefs');
+            return;
+        }
+
+        const sportCode = args[0];
+        const teamName = args.slice(1).join(' ').toLowerCase();
+
+        let sportTitle;
+        let sportDesc;
+        let sportGroup;
+        let sportKey;
+
+        switch (sportCode.toLowerCase()) {
+            case 'nfl':
+                sportTitle = 'Football';
+                sportDesc = 'NFL';
+                sportGroup = 'Football';
+                sportKey = 'americanfootball_nfl';
+                break;
+            // Add more cases for other sports
+            default:
+                message.channel.createMessage('Invalid sport code! Please provide a valid sport code for odds.');
+                return;
+        }
+
+        const response = await got.get(`https://api.the-odds-api.com/v4/sports/${sportKey}/odds/?apiKey=${api_key}&regions=us&markets=h2h,spreads&oddsFormat=american`, {
             responseType: 'json',
         });
-        const sportsData = response.body;
-        const activeSports = sportsData.filter((sport) => sport.active);
-        const sportsFields = activeSports.map((sport) => {
-            const sportTitle = sport.title;
-            const sportDesc = sport.description;
-            const sportGroup = sport.group;
-            const sportOutrights = sport.has_outrights;
-            return {
-                name: `${sportTitle} / ${sportDesc}`,
-                value: `Sport: ${sportGroup}`,
+        const oddsData = response.body;
+
+        const teamOdds = oddsData.find((odds) => {
+            const homeTeam = odds.home_team.toLowerCase();
+            const awayTeam = odds.away_team.toLowerCase();
+            const startDate = new Date(odds.commence_time);
+            const options = {
+              timeZone: 'America/New_York',
+              month: '2-digit',
+              day: '2-digit',
+              year: 'numeric',
             };
+            const formattedDate = startDate.toLocaleString('en-US', options);
+            return homeTeam.includes(teamName) || awayTeam.includes(teamName);
         });
-        message.channel.createMessage({
-            embed: {
-                title: `Active Sports/Leagues With Available Betting Odds:`,
-                fields: sportsFields,
-                color: 2123412,
-            },
-        });
-    } catch (error) {
+
+        if (!teamOdds) {
+            message.channel.createMessage(`No odds found for ${teamName} in ${sportTitle}.`);
+            return;
+        }
+
+        const filteredBookmakers = ['DraftKings', 'FanDuel'];
+
+        const oddsFields = [];
+
+        teamOdds.bookmakers
+            .filter((bookmaker) => filteredBookmakers.includes(bookmaker.title))
+            .forEach((bookmaker) => {
+                bookmaker.markets
+                    .filter((market) => market.last_update.includes('2023')) // Modify the condition to filter recent games based on the last_update field
+                    .forEach((market) => {
+                        const homeOutcome = market.outcomes.find((outcome) => outcome.name === teamOdds.home_team);
+                        const awayOutcome = market.outcomes.find((outcome) => outcome.name === teamOdds.away_team);
+
+                        if (homeOutcome && awayOutcome) {
+                            const bookmakerTitle = bookmaker.title;
+                            const homeOdds = homeOutcome.price;
+                            const awayOdds = awayOutcome.price;
+
+                            const oddsInfo = {
+                                name: `Bookmaker: ${bookmakerTitle}`,
+                                value: `Home: ${teamOdds.home_team} -> ${homeOdds}\nAway: ${teamOdds.away_team} -> ${awayOdds}`,
+                            };
+
+                            oddsFields.push(oddsInfo);
+                        }
+                    });
+            });
+            message.channel.createMessage({
+                embed: {
+                  title: `Betting Odds for ${sportDesc} (${sportTitle}) - ${teamOdds.home_team} vs ${teamOdds.away_team}`,
+                  fields: oddsFields,
+                  color: 2123412,
+                },
+              });
+    } 
+    catch (error) {
         console.error(error);
     }
 });
